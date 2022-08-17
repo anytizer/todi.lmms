1bd3711a8563049078750a33d6fadf2c401b659f
Bimal Poudel <anytizer@users.noreply.github.com>
2022-08-17 11:40:44 -0600

    Create Thaats.cs

---
+++ libraries/Thaats.cs
@@ -0,0 +1,63 @@
+namespace libraries
+{
+    public class Thaats
+    {
+        // https://en.wikipedia.org/wiki/Bilaval_(thaat)
+        // S R G M P D N S
+        public string BILAVAL = "SA RE GA MA PA DHA NI SA*"; // All pure notes
+
+
+        // https://en.wikipedia.org/wiki/Kalyan_(thaat)
+        // S R G M' P D N S
+        public string KALYAN = "SA RE GA MA' PA DHA NI SA*"; // MA’ tivra | bhupali => "SA RE GA PA DHA SA*"
+        // Yaman, Bhupali, Hindol, Kedar, Shuddha Kalyan, Shyam Kalyan,
+        // Yaman Kalyan, Khem Kalyan, Savani Kalyan Chhayanat,
+        // Hameer, Gaud Sarang, Kamod, Maru Bihag, Nand
+
+
+        // https://en.wikipedia.org/wiki/Khamaj_(thaat)
+        // S R G M P D n S
+        public string KHAMAJ = "SA RE GA MA PA DHA ni SA*"; // ni komal
+
+        // https://en.wikipedia.org/wiki/Bhairav_(thaat)
+        // S r G M P d N S
+        public string BHAIRAV = "SA re GA MA PA dha NI SA*"; // re and dha komal
+
+
+        // https://en.wikipedia.org/wiki/Poorvi_(thaat)
+        // S r G M' P d N S
+        public string POORVI = "SA re GA MA' PA dha NI SA*"; // re and dha komal and MA’ tivra
+
+
+        // https://en.wikipedia.org/wiki/Marva_(thaat)
+        // S r G M' P D N S	
+        public string MARVA = "SA re GA MA' PA DHA NI SA*"; // re komal and MA’ tivra
+
+
+        // https://en.wikipedia.org/wiki/Kafi_(thaat)
+        // S R g M P D n S
+        public string KAFI = "SA RE ga MA PA DHA ni SA*"; // ga and ni komal
+
+
+        // https://en.wikipedia.org/wiki/Asavari_(thaat)
+        // S R g M P d n S
+        public string ASAVARI = "SA RE ga MA PA dha ni SA*"; // ga, dha and ni komal
+
+
+        // https://en.wikipedia.org/wiki/Bhairavi_(thaat)
+        // S r g M P d n S
+        public string BHAIRAVI = "SA re ga MA PA dha ni SA*"; // re, ga, dha and ni komal
+
+
+        // https://en.wikipedia.org/wiki/Todi_(thaat)
+        // S r g M' P d N S
+        public string TODI = "SA re ga MA' PA dha NI SA*"; // re, ga and dha komal and MA’ tivra
+
+
+        // BILAVAL
+        // https://en.wikipedia.org/wiki/Bilaval
+        // https://en.wikipedia.org/wiki/Bilaval_(thaat)
+        // Arohi: Sa, Re, Ga, ma, Pa, Dha, Ni, Sa'
+        // Avarahi: Sa' Ni Dha, Pa, ma Ga, Ma, Re Sa
+    }
+}
